0a95ab1afba7dfc0bf360f6c42ce450661cc8187
Matias Sykuler <matisyk@hotmail.com>
2023-03-02 18:19:30 -0700

    feat: update settle up logic

--- pages/settle-up.tsx
+++ pages/settle-up.tsx
@@ -7,37 +7,66 @@ import {
   ListItemText,
   Stack,
   Typography,
-} from "@mui/material";
-import { useState } from "react";
+} from '@mui/material';
+import { useState } from 'react';
 
-interface SettleUpProps {}
+interface SettleUpProps {
+  ides: number;
+  addresses: [string];
+}
+
+interface Debt {
+  id: number;
+  address: string;
+  name: string;
+  amount: number;
+}
 
 const SettleUp: React.FC<SettleUpProps> = () => {
-  const [selectedDebts, setSelectedDebts] = useState<Array<number>>([]);
-  const [clickedBox, setClickedBox] = useState();
+  const [selectedDebts, setSelectedDebts] = useState<Array<Debt>>([]);
 
-  const handleChange = (index: number) => {
-    const updateList = [...selectedDebts, index];
-    setSelectedDebts(updateList);
+  const handleChange = (debt: Debt, checked: boolean) => {
+    let result = [...selectedDebts];
+
+    if (checked) {
+      result.push(debt);
+    } else {
+      result.splice(
+        selectedDebts.findIndex((d) => d == debt),
+        1
+      );
+    }
+
+    setSelectedDebts(result);
   };
-  // const handleClick = ()=>{
-  //     if(clickBox){
-  //         setClickBox()
-  //     }
-  // };
+
+  const save = () => {
+    const debtsToSettleUp = selectedDebts
+      .map(({ address, amount }) => {
+        return { address, amount };
+      });
+      console.log(debtsToSettleUp);
+      
+  };
 
   const debts = [
     {
-      name: "Bob",
+      name: 'Bob',
       amount: 10,
+      id: 0,
+      address: '0x0',
     },
     {
-      name: "Rick",
+      name: 'Rick',
       amount: 88,
+      id: 1,
+      address: '0x1',
     },
     {
-      name: "Patrick",
+      name: 'Patrick',
       amount: 67,
+      id: 2,
+      address: '0x2',
     },
   ];
 
@@ -45,33 +74,26 @@ const SettleUp: React.FC<SettleUpProps> = () => {
     <Stack>
       <Typography variant="h2">Settle up</Typography>
       <Stack direction="column" gap={2} p={2}>
-        {debts.map(({ name, amount }, index) => (
+        {debts.map((debt, index) => (
           <ListItemButton key={index}>
             <Checkbox
-              checked={selectedDebts.includes(index)}
-              onChange={() => handleChange(index)}
+              checked={!!selectedDebts.find(d => d.address === debt.address)}
+              onChange={(event) => handleChange(debt, event.target.checked)}
             />
-            <ListItemText primary={`${name} $${amount}`} />
+            <ListItemText primary={`${debt.name} $${debt.amount}`} />
           </ListItemButton>
         ))}
       </Stack>
       <Typography variant="h4" p={2}>
-        You will settle up with{" "}
-        {debts[selectedDebts[0]]
-          ? debts
-              .filter((debt, index) => selectedDebts.includes(index))
-              .map(({ name }) => name)
-              .join(", ")
-          : null}{" "}
-        for $
-        {debts[selectedDebts[0]]
-          ? debts
-              .filter((debt, index) => selectedDebts.includes(index))
-              .map(({ amount }) => amount)
-              .reduce((accum, current) => accum + current, 0)
-          : null}
+        You will settle up with{' '}
+        {selectedDebts.map(({ name }) => name).join(', ')} for $
+        {selectedDebts
+          .map(({ amount }) => amount)
+          .reduce((accum, current) => accum + current, 0)}
       </Typography>
-      <Button variant="contained">Settle up</Button>
+      <Button onClick={save} variant="contained">
+        Settle up
+      </Button>
     </Stack>
   );
 };
