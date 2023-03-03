ac35f36690d352ac228feac1607ec24863ceb475
Brian Sasbon <bsasbon@neopower.digital>
2023-03-02 23:40:07 -0700

    feat: add open sans font

--- styles/fonts.style.ts
+++ styles/fonts.style.ts
@@ -1,18 +1,19 @@
-import { Exo_2, Roboto_Mono } from 'next/font/google';
+import { Exo_2, Roboto_Mono, Open_Sans } from 'next/font/google';
 
 const robotoMono = Roboto_Mono({ subsets: ['latin'] });
 const exo2 = Exo_2({ subsets: ['latin'] });
+const openSans = Open_Sans({ subsets: ['latin'] });
 
 export const bodyFonts = {
-  fontFamily: exo2.style.fontFamily,
+  fontFamily: openSans.style.fontFamily,
 };
 
 export const headingFonts = {
-  fontFamily: exo2.style.fontFamily,
+  fontFamily: openSans.style.fontFamily,
 };
 
 export const buttonFonts = {
-  fontFamily: exo2.style.fontFamily,
+  fontFamily: openSans.style.fontFamily,
 };
 
 export const currencyFonts = {
