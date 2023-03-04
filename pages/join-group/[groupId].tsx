e29fe1af82f3038ea545482372173570aafcebbf
Matias Sykuler <matisyk@hotmail.com>
2023-03-04 11:41:00 -0700

    feat: add group states to join group

--- pages/join-group/[groupId].tsx
+++ pages/join-group/[groupId].tsx
@@ -1,22 +1,28 @@
-import { LoadingButton } from "@mui/lab";
-import { Stack, TextField, Typography } from "@mui/material";
-import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import WalletWidget from '@/components/WalletWidget';
+import { accountAtom } from '@/states/account.atom';
+import { LoadingButton } from '@mui/lab';
+import { Stack, TextField, Typography } from '@mui/material';
+import { useAtom } from 'jotai';
+import { useRouter } from 'next/router';
+import { useRef, useState } from 'react';
 
 interface JoinGroupProps {}
 
 const JoinGroup: React.FC<JoinGroupProps> = () => {
+  const [account, _] = useAtom(accountAtom);
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const { groupId } = router.query;
 
   // TODO: Replace with DB data
-  const memberName = useRef("");
-  const groupName = "ETH Denver";
+  const memberName = useRef('');
+  const groupName = 'ETH Denver';
 
   const handleClick = () => {
     // TODO: Add member with provided name
     console.log({ name: memberName.current, groupId });
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 2000);
   };
 
   return (
@@ -37,15 +43,18 @@ const JoinGroup: React.FC<JoinGroupProps> = () => {
           memberName.current = event.target.value;
         }}
       />
-
-      <LoadingButton
-        loading={isLoading}
-        variant="contained"
-        onClick={handleClick}
-        loadingPosition="end"
-      >
-        <span>{isLoading ? "Sending TX..." : "Join"}</span>
-      </LoadingButton>
+      {account ? (
+        <LoadingButton
+          loading={isLoading}
+          variant="contained"
+          onClick={handleClick}
+          loadingPosition="end"
+        >
+          <span>{isLoading ? 'Sending TX...' : 'Join'}</span>
+        </LoadingButton>
+      ) : (
+        <WalletWidget />
+      )}
     </Stack>
   );
 };
